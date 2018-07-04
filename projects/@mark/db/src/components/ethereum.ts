8859594907b76812bb17f729f84a9cee9f76730a
Jake Ferrante <jake.ferrante@hotmail.com>
2018-07-04 16:58:20 -0400

    Add custom web3 typings Fix build errors

--- projects/@mark/db/src/components/ethereum.ts
+++ projects/@mark/db/src/components/ethereum.ts
@@ -1,70 +1,125 @@
-import Web3 = require('web3');
-import { Block, Transaction, TransactionReceipt, Signature } from 'web3/types';
+// const Web3 = require('web3');
+import * as web3 from 'web3';
+import { BlockWithTransactionData, Transaction, TransactionReceipt } from 'ethereum-types';
+// import { Block, Transaction, TransactionReceipt, Signature, } from 'web3/types';
+// import { Block, Transaction, TransactionReceipt,  } from 'web3/types';
+// import { Block, Transaction, TransactionReceipt } from 'web3';
+
 import { EthereumPost } from '../models/EthereumPost';
 import { getInstance } from './web3';
+import { resolve } from 'url';
 
 const txValue = 1;
 const txGasPrice = 1;
 const ethChainId = 112358;
 
-// Signs and sends a transaction to the Ethereum network.
-export function addEthereumPost(post: EthereumPost, address: string, key: string): Promise<TransactionReceipt> {
-
-    return createRawTx(post, address, key)
-        .then((rawTx: string) => {
-            return getInstance().eth.sendSignedTransaction(rawTx);
-        })
-        .then((receipt: any) => {
-            return Promise.resolve(receipt);
-        });
+/**
+ * Signs and sends a transaction to the Ethereum network.
+ * @param post mark post item
+ * @param address ethereum wallet to post with
+ * @param key key to sign with
+ */
+export function addEthereumPost(post: EthereumPost, address: string, key: string): Promise<string> {
+    return new Promise((resolve, reject) => {
+        createRawTx(post, address, key)
+            .then((rawTxData: string) => {
+                return getInstance().eth.sendRawTransaction(rawTxData, (error, transactionHash) => {
+                    if (error) {
+                        reject(error);
+                    } else {
+                        resolve(transactionHash);
+                    }
+                });
+            });
+    });
 }
 
 // Returns undefined if the transaction does not contain a valid EthereumPost.
 export function getEthereumPost(txHash: string): Promise<EthereumPost> {
-
-    return getInstance().eth.getTransaction(txHash)
-        .then((tx: Transaction) => {
+    return new Promise((resolve, reject) => {
+        getInstance().eth.getTransaction(txHash, (error, tx) => {
+            if (error) {
+                reject(error);
+            }
 
             if (tx.input !== '0x') {
                 const post: EthereumPost = <EthereumPost>JSON.parse(getInstance().utils.toAscii(tx.input));
                 return Promise.resolve(post);
             }
         });
+    });
 }
 
 // Return the most recently published block
-export function getNewestBlock(): Promise<Block> {
+export function getNewestBlock(): Promise<BlockWithTransactionData> {
+    const returnTransactionObjects = true;
 
-    return getInstance().eth.getBlockNumber()
-        .then(blockNum => {
-            return getInstance().eth.getBlock(blockNum);
+    return new Promise((resolve, reject) => {
+
+        getInstance().eth.getBlockNumber((error, blockNumber) => {
+            if (error) {
+                reject(error);
+            } else {
+                getInstance().eth.getBlock(blockNumber, returnTransactionObjects, (error, blockObj) => {
+                    if (error) {
+                        reject(error);
+                    } else {
+                        resolve(blockObj);
+                    }
+                });
+            }
+
         });
+    });
 }
 
-// Returns a raw signed transaction string
-function createRawTx(post: EthereumPost, address: string, key: string): Promise<string> {
+export function getBlock(blockHashOrBlockNumber: string | number): Promise<BlockWithTransactionData> {
+    return new Promise((resolve, reject) => {
+        getInstance().eth.getBlock(blockHashOrBlockNumber, true, (error, blockObj) => {
+            if (error) {
+                reject(error);
+            } else {
+                resolve(blockObj);
+            }
+        });
+    });
+}
+
+/**
+ *
+ * @param post item being posted
+ * @param address address (wallet) being posted with
+ * @param privateKey address' private key
+ */
+function createRawTx(post: EthereumPost, address: string, privateKey: string): Promise<string> {
 
     const inputData: string = getInstance().utils.toHex(JSON.stringify(post));
     const txGas = 21000 + (68 * (inputData.length / 2));
 
-    const tx = {
+    const tx: Transaction = {
         to: address,
         value: txValue,
         gas: txGas,
         gasPrice: txGasPrice,
-        chainId: ethChainId,
+        chainId: ethChainId, // chainId is not in transaction structure per typings (web3 expects it);
         data: inputData
         // 'from' automatically set to address
         // 'nonce' automatically calculated
-    };
+    } as any;
 
-    return (<Promise<string>> getInstance().eth.accounts.signTransaction(tx, key, false))
-        .then((signedTx: string) => {
+    return getInstance().eth.accounts.signTransaction(tx, privateKey)
+        .then(signedTx => {
 
-            // Format the signed transaction
-            const validJson = JSON.stringify(signedTx);
-            const rawTx = JSON.parse(validJson).rawTransaction;
+            try {
+                // Format the signed transaction
+                const validJson = JSON.stringify(signedTx);
+                const rawTx = JSON.parse(validJson).rawTransaction;
 
-            return Promise.resolve(rawTx);
+                return Promise.resolve(rawTx);
+
+            } catch (error) {
+                return Promise.reject(error);
+            }
         });
+
 }
